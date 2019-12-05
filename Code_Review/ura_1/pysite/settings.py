f8889199586cfdeb347621246ed0ef9f4f17ad5c
yuuta3594 <yuuta3594@outlook.jp>
2019-12-06 05:05:22 +0900

    add markdownx

--- Code_Review/ura_1/pysite/settings.py
+++ Code_Review/ura_1/pysite/settings.py
@@ -20,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'markdownx',
+
     'core.apps.CoreConfig',
     'site_page.apps.SitePageConfig',
 ]
@@ -101,3 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Code_Review/ura_1/pysite/urls.py
+++ Code_Review/ura_1/pysite/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
     path('core/', include('core.urls')),
     path('', include('site_page.urls')),
 ]
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
